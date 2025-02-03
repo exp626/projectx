f330ab3f8307111bc433b399fbacfe213f9aa135
Andrey Chernov <dad@boba.ru>
2025-02-03 22:47:23 +0300

    Start create generator.

--- protocol/generator/generator/generator.go
+++ protocol/generator/generator/generator.go
@@ -3,21 +3,49 @@ package generator
 import (
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"os"
+	"text/template"
 )
 
 // структура для парсинга протокола
 type Protocol struct {
-	Commands map[string]interface{} `json:"commands"`
-	Structs  map[string]interface{} `json:"structs"`
+	Version  string    `json:"version"`
+	Protocol string    `json:"protocol_name"`
+	Commands []Command `json:"commands"`
 }
 
 type Command struct {
-	CommandCode string                 `json:"command_code"`
-	
+	CommandCode int    `json:"command_code"`
+	Body        Body   `json:"body"`
+	Description string `json:"description"`
 }
 
-func GenerateProtocol(filePath string) {
+type Body struct {
+	Type        string  `json:"type"`
+	Direction   string  `json:"direction"`
+	Description string  `json:"description"`
+	Name        string  `json:"name"`
+	Options     Options `json:"options"`
+}
+
+type Options struct {
+	Fields []Field `json:"fields"`
+}
+
+type Field struct {
+	Name        string `json:"name"`
+	Type        string `json:"type"`
+	Description string `json:"description"`
+}
+
+type Types struct {
+	Name    string  `json:"name"`
+	Type    string  `json:"type"`
+	Options Options `json:"options"`
+}
+
+func GenerateProtocol(filePath string, templatePath string, outputPath string) {
 	jsonFile, err := os.Open(filePath)
 	if err != nil {
 		fmt.Println("Error opening protocol file:", err)
@@ -25,12 +53,44 @@ func GenerateProtocol(filePath string) {
 	}
 	defer jsonFile.Close()
 
-	var protocol map[string]interface{}
+	var protocol Protocol
 	err = json.NewDecoder(jsonFile).Decode(&protocol)
 	if err != nil {
 		fmt.Println("Error decoding protocol file:", err)
 		return
 	}
 
-	fmt.Println(protocol)
+	// генерируем фаил с командами
+	generateCommands(protocol, templatePath, outputPath)
+
+	// генерируем фаил с типами
+	generateTypes(protocol, templatePath, outputPath)
+}
+
+func generateCommands(protocol Protocol, templatePath string, outputPath string) {
+	// генерируем фаил с командами
+
+	slog.Info("Generating commands", "templatePath", templatePath, "outputPath", outputPath)
+
+	tmpl, err := template.ParseFiles(templatePath)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+	tmpl.Execute(os.Stdout, protocol)
+}
+
+func generateTypes(protocol Protocol, templatePath string, outputPath string) {
+	// генерируем фаил с типами
+
+	slog.Info("Generating types", "templatePath", templatePath, "outputPath", outputPath)
+
+	tmpl, err := template.ParseFiles(templatePath)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+	tmpl.Execute(os.Stdout, protocol)
 }
--- protocol/generator/main.go
+++ protocol/generator/main.go
@@ -10,14 +10,30 @@ import (
 // фаил протокола получаем из параметров запуска
 
 var fileName string
+var templatePath string
+var outputPath string
 
 func main() {
 	flag.StringVar(&fileName, "f", "protocol.json", "file name")
+	flag.StringVar(&templatePath, "t", "templates", "template path")
+	flag.StringVar(&outputPath, "o", "generated", "output path")
+
+	flag.Parse()
 
 	if fileName == "" {
 		fmt.Println("file name is required")
 		os.Exit(1)
 	}
 
-	generator.GenerateProtocol(fileName)
+	if outputPath == "" {
+		fmt.Println("output path is required")
+		os.Exit(1)
+	}
+
+	if templatePath == "" {
+		fmt.Println("template path is required")
+		os.Exit(1)
+	}
+
+	generator.GenerateProtocol(fileName, templatePath, outputPath)
 }
